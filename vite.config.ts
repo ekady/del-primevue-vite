795983941ed2ef3639a8317898b06a4fdf0db072
ekady <dyahekalestari9@gmail.com>
2024-12-31 14:36:38 +0700

    chore: adjust eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+
 import VueI18n from '@intlify/unplugin-vue-i18n/vite';
-import Components from 'unplugin-vue-components/vite';
 import { PrimeVueResolver } from '@primevue/auto-import-resolver';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
